63b9515539f29947f50ae7016728d04ed380855b
cataclysm1987 <conklin.eric@gmail.com>
2017-02-19 16:01:27 -0500

    add files and complete initial model

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+﻿function CalculateTriangleArea() {
+    var one = parseInt(document.getElementById("side1").value);
+    var two = parseInt(document.getElementById("side2").value);
+    var three = parseInt(document.getElementById("side3").value);
+
+    var sum = one + two + three;
+    var s = (one + two + three) / 2;
+    var s1 = s - one;
+    var s2 = s - two;
+    var s3 = s - three;
+    var aSquared = s * s1 * s2 * s3;
+
+    var A = Math.sqrt(aSquared);
+    if (isNaN(A) || A == 0 || one == 0 || two == 0 || three == 0) {
+        document.getElementById("answer1").innerHTML = "Invalid Triangle";
+    } else {
+        document.getElementById("answer1").innerHTML = "Your Triangle Area is: " + A;
+    }
+}
